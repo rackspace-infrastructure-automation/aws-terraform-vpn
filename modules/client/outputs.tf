6e138d2f41526626482858013467f15b29ac8c42
John Titus <john.titus@rackspace.com>
2020-02-04 14:26:11 -0600

    Sort outputs in alphabetical order

--- modules/client/outputs.tf
+++ modules/client/outputs.tf
@@ -1,10 +1,10 @@
-output "aws_ec2_client_vpn_endpoint_id" {
-  description = "client vpn end point id"
-  value       = aws_ec2_client_vpn_endpoint.client_vpn.id
-}
-
 output "aws_ec2_client_vpn_endpoint_dns" {
   description = "client vpn end point DNS"
   value       = aws_ec2_client_vpn_endpoint.client_vpn.dns_name
+}
+
+output "aws_ec2_client_vpn_endpoint_id" {
+  description = "client vpn end point id"
+  value       = aws_ec2_client_vpn_endpoint.client_vpn.id
 }
 
